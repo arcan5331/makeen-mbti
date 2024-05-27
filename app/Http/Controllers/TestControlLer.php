03c609211d7d6e973638c229530e9d4721d36f50
arcan5331 <alighaforian@yahoo.com>
2024-05-27 10:53:59 +0330

    updated show and fill functions in TestControlLer

--- app/Http/Controllers/TestControlLer.php
+++ app/Http/Controllers/TestControlLer.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TestResource;
 use App\Models\Test;
 use Illuminate\Http\Request;
 
@@ -9,9 +10,12 @@ class TestControlLer extends Controller
 {
     public function show($test_name)
     {
+        return new TestResource(Test::where('name', $test_name)->sole());
     }
 
     public function fill($test_name)
     {
+        // todo: make the test filling part
+        return ["message" => 'its under development'];
     }
 }
