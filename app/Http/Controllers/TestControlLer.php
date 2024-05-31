f1f4d9f1c86f8f1511975c48f1801183b120dc7f
arcan5331 <alighaforian@yahoo.com>
2024-05-31 20:52:36 +0330

    used TestScoringService to calculate user score in fill method.

--- app/Http/Controllers/TestControlLer.php
+++ app/Http/Controllers/TestControlLer.php
@@ -5,10 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FilledTestFormRequest;
 use App\Http\Resources\TestResource;
 use App\Models\Test;
+use App\Services\TestScoringService;
 use Illuminate\Http\Request;
 
 class TestControlLer extends Controller
 {
+    private TestScoringService $scoringService;
+
+    public function __construct(TestScoringService $scoringService)
+    {
+        $this->scoringService = $scoringService;
+    }
+
     public function show(Test $test)
     {
         return new TestResource($test);
@@ -16,7 +24,10 @@ class TestControlLer extends Controller
 
     public function fill(FilledTestFormRequest $request, Test $test)
     {
-        // todo: make the test filling part
-        return ["message" => 'its under development'];
+        $userScores = $this->scoringService->calculateUserScores($test, $request->input('answers'));
+        $userType = $this->scoringService->calculateUserType($test, $request->input('answers'));
+        $userTypePercent = $this->scoringService->calculatePairTypePercentages($test, $request->input('answers'));
+        return [$userTypePercent, $userScores, $userType];
+
     }
 }
